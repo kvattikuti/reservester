eb796b88ed931f36c39bc7d7da9446a795b8dd68
kvattikuti <kalyana.vattikuti@gmail.com>
2014-07-22 10:32:32 -0400

    addition of unit tests for the mailer

--- spec/mailers/reservation_mailer_spec.rb
+++ spec/mailers/reservation_mailer_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe ReservationMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context "send mailer" do
+		before :each do 
+			@restaurant = FactoryGirl.create(:restaurant)
+  			@message = ReservationMailer.notify_owner_of_reservation @restaurant.reservations.first
+		end
+
+  		it "contains restaurant name" do
+  			expect(@message.body).to include(@restaurant.name)
+  		end
+  		it "contains reservation email" do
+  			expect(@message.body).to include(@restaurant.reservations.first.email)
+  		end
+  		it "contains reservation datetime" do
+  			expect(@message.body).to include(@restaurant.reservations.first.requested_datetime)
+  		end
+  		it "contains reservation message" do
+  			expect(@message.body).to include(@restaurant.reservations.first.message)
+  		end
+  		it "sent to the owner" do
+  			expect(@message.to).to include(@restaurant.owner.email)
+  		end
+	end
 end
