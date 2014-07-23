499859cd2fb180f52dcc47ae29c36c0d1ad82fcc
kvattikuti <kalyana.vattikuti@gmail.com>
2014-07-23 11:49:57 -0400

    owner dashboard

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,6 +1,7 @@
 class OwnersController < ApplicationController
 	def dashboard
 		print "dashboard action called"
+		@restaurants = current_owner.restaurants
 	end 
 
 	before_action :authenticate_owner!, only: [:dashboard]
